fca874ad6685cfdee8375d8075349925975a0414
k-shunji <k-shunji@flect.co.jp>
2013-08-21 14:37:18 +0900

    Bug fix

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,11 @@ package controllers
 
 import play.api.mvc.Action;
 import play.api.mvc.Controller;
+import play.api.mvc.AnyContent;
+import play.api.mvc.Request;
+import play.api.mvc.Result;
+import play.api.Play;
+import play.api.Play.current;
 import java.io.File;
 import play.api.data.Form;
 import play.api.data.Forms.tuple;
@@ -15,7 +20,15 @@ object Application extends Controller {
     Redirect("/editor");
   }
 
-  def editor = Action { implicit request =>
+  def filterAction(f: Request[AnyContent] => Result): Action[AnyContent] = Action { request =>
+    if (Play.isProd && request.headers.get("x-forwarded-proto").getOrElse("http") == "http") {
+      Redirect("https://" + request.host + request.path);
+    } else {
+      f(request);
+    }
+  }
+  
+  def editor = filterAction { implicit request =>
     val bootstrap = request.getQueryString("bootstrap").getOrElse("true").toBoolean;
     val template = request.getQueryString("template") match {
       case Some(s) if (new File("app/data/" + s).exists) => s;
@@ -31,7 +44,7 @@ object Application extends Controller {
     )
   );
   
-  def validate = Action { implicit request =>
+  def validate = filterAction { implicit request =>
     val form = validateForm.bindFromRequest;
     if (form.hasErrors) {
       BadRequest;
@@ -42,7 +55,7 @@ object Application extends Controller {
     }
   }
   
-  def downloadText = Action { implicit request =>
+  def downloadText = filterAction { implicit request =>
     request.body.asFormUrlEncoded.flatMap {
       _.get("text").map(_.head)
     } match {
--- app/controllers/Salesforce.scala
+++ app/controllers/Salesforce.scala
@@ -102,7 +102,6 @@ object Salesforce extends Controller {
   def create = filterAction { implicit request =>
     val form = createForm.bindFromRequest;
     if (form.hasErrors) {
-println("Error: " + form.errors);
       BadRequest;
     } else {
       val (name, label, desc, json, useEmailField, useUrlField) = form.get;
